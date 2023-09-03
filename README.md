a4e35329313ba84fe33c7a608722bd5ad4848160
Remix <32577626+RemixFusion@users.noreply.github.com>
2023-09-02 22:12:35 -0400

    Final release

---
+++ README.md
@@ -0,0 +1,15 @@
+# UFW Script for TCPShield
+
+These scripts are designed to replace the default behavior of TCPShield's IPSet and utilize UFW (IPTables wrapper) Firewall instead. When UFW and IPSet are enabled, IPSet does not take prescident, requiring a UFW rule to allow access to backend servers. While UFW can be configured to only allow from appropriate IP addresses to these backends, this script will automatically handle creating that ruleset and updating it regularly. You do not need IPSet with this script.
+
+## Configuration
+Download the tcpshield.sh file and place it in a directory on your server.
+
+Run the following command to make the file executable:
+```
+chmod u+x tcpshield.sh
+```
+
+Test the script by running ./tcpshield (You may want to run it twice and check ufw status to ensure rules are still present)
+
+Create a Cron to automatically run the script at whatever interval you wish.
---
+++ remixed.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+APPNAME="Minecraft"
+IPFILE=/usr/local/remixed-v4.txt
+UFW_RULE_PREFIX="Allow Remixed Access to Minecraft Server"
+
+# Download the list of IPs from the website
+wget https://vps1.ny.remixed.pro/v4 -O "$IPFILE"
+
+# Function to validate CIDR notation
+is_valid_cidr() {
+  local cidr="$1"
+  local ip
+  local mask
+  IFS="/" read -r ip mask <<< "$cidr"
+  if [[ $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]] && [[ $mask =~ ^[0-9]+$ ]] && ((mask >= 0 && mask <= 32)); then
+    return 0  # CIDR is valid
+  else
+    return 1  # CIDR is invalid
+  fi
+}
+
+# Loop through the IPs in the file and validate them
+while read -r IP; do
+  # Trim leading and trailing whitespace
+  IP=$(echo "$IP" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+
+  # Check if the line is empty or starts with a comment character (#)
+  if [[ -z $IP || $IP =~ ^# ]]; then
+    continue
+  fi
+
+  if is_valid_cidr "$IP"; then
+    if ! ufw status | grep -q "$UFW_RULE_PREFIX" | grep -q "$IP"; then
+      # IP is not in the current list, add it to UFW
+      ufw allow from "$IP" to any app "$APPNAME" comment "$UFW_RULE_PREFIX"
+    fi
+  else
+    echo "Invalid CIDR notation: $IP"
+  fi
+done < "$IPFILE"
+
+# Remove IPs from UFW that are no longer in the list
+ufw status | grep -o "$UFW_RULE_PREFIX from [0-9./]\+" | awk '{print $3}' | sort | uniq | while read -r IP; do
+  if ! grep -q "$IP" "$IPFILE"; then
+    ufw delete allow from "$IP" to any app "$APPNAME" comment "$UFW_RULE_PREFIX"
+  fi
+done
